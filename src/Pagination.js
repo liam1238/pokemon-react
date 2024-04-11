1225de3d4fcdfd796a2872cfd69480c58b94ec42
Liam Golan <liamgolan@Liams-MacBook-Pro.local>
2024-04-11 21:12:34 +0300

    add tailwind css and style

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Button from './ui/button';
 
 export default function Pagination({ goToNextPage, goToPrevPage }) {
-  return (
-    <div>
-      {goToPrevPage && <button onClick={goToPrevPage}>Previus</button>}
-      <button onClick={goToNextPage}>Next</button>
+  return (  
+    <div className='pl-3 text-center'>
+        {goToPrevPage && <Button text="Previeus" onClick={goToPrevPage}/> }
+        {goToNextPage && <Button text="Next" onClick={goToNextPage}/> }
     </div>
   )
 }
